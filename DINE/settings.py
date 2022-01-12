a327f8844e5442dd094dd4ef7d42257a4779380c
nigussolomon <nigussaved@gmail.com>
2022-01-12 20:16:24 +0300

    register form and login form template and style complete

--- DINE/settings.py
+++ DINE/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main.apps.MainConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -117,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'home'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- main/views.py
+++ main/views.py
@@ -3,22 +3,21 @@ from django.shortcuts import redirect, render
 from main.forms import Registration
 from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
+
+def home(request):
+    return render(request, 'main/home.html')
+    
 def register(request):
     if request.method == 'POST':
         form = Registration(request.POST)
-        formLogin= AuthenticationForm(request.POST)
         if form.is_valid():
             form.save()
             form.cleaned_data
             return redirect('login')
-        elif formLogin.is_valid():
-            formLogin.save()
-            formLogin.cleaned_data
-            
     else:
         form= Registration()
         formLogin=AuthenticationForm()
-    return render(request, 'main/home.html',{'form':form})
+    return render(request, 'main/register.html',{'form':form})
 
 
 

74da1e5563d753dbf572ce7e9ee4dac604034b5b
nigussolomon <nigussaved@gmail.com>
2022-01-12 20:25:54 +0300

    some more styling on the forms

---
+++ main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views as mainViews
+
+urlpatterns = [
+    path('',mainViews.home, name = 'home' ),
+    path('login/', auth_views.LoginView.as_view(template_name = 'main/login.html'), name='login'),
+    path('signup/', mainViews.register, name='register'),
+]

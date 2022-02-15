e9c994aac5927ead656b368e061ecac1450c12a8
nigussolomon <nigussaved@gmail.com>
2022-02-15 21:42:53 +0300

    completed ui for displaying menu items

--- menu/views.py
+++ menu/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import redirect, render
-from menu.models import Profile
-from .forms import AddContactInfo, UpdateProfile, RestaurantForm
+from menu.models import Menu, Profile, Restaurant
+from django.contrib.auth.models import User
+from .forms import AddContactInfo, UpdateProfile, RestaurantForm, AddtoMenu
 
 # Create your views here.
+
+def home(request):
+    context = {
+        'restaurants':User.objects.all()
+    }
+    return render (request, 'menu/homeview.html', context)
+
 
 def profile(request):
     if request.method == 'POST':
@@ -27,4 +35,26 @@ def profile(request):
     return render(request, 'menu/profile.html', {'userUpdate': userUpdateForm, 'addContact':addContactInfo, 'restaurant': restaurantForm} )
 
 def add_to_menu(request):
-    return render(request, 'menu/add_to_menu.html')
+    if request.method == 'POST':
+        addForm = AddtoMenu(request.POST)
+
+        if addForm.is_valid():
+            addForm.instance.restaurant = request.user.restaurant
+            addForm.save()
+            return redirect('menuup')
+    else:
+        addForm = AddtoMenu(instance = request.user.restaurant)
+
+    context = {
+        'food' : Menu.objects.all(),
+        'addform':addForm
+    }
+    return render(request, 'menu/add_to_menu.html', context)
+
+
+def menu(request):
+    context = {
+        'food' : Menu.objects.all(),
+        'user' : User.objects.all(),
+    }
+    return render(request, 'menu/menu.html', context)

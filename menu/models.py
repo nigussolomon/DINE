77549a32e29e277c05a712f791ae9c56113bf2a6
tmc <hawibek223@gmail.com>
2022-02-15 10:41:07 +0300

    completed Restaurant Database issue

--- menu/models.py
+++ menu/models.py
@@ -11,3 +11,15 @@ class Profile(models.Model):
     
     def __str__(self):
         return f"{self.user.username}'s Profile"
+    
+class Restaurant(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_regex = RegexValidator(regex= r'^\+?1?\d{9,15}$', message = "Phone number must be entered in the format: '+999999999' . Up to 15 digits allowed. ") 
+    restaurant_name = models.CharField(default= "Enter Your Restaurant name ", max_length= 40)
+    phone_number = models.CharField(default = 'please add your number', validators=[phone_regex],max_length=17, blank= False)
+
+def __str__(self):
+        return f"{self.restarant_name}'s Restaurant"
+    
+class Menu(models.Model):
+    restaurant= models.ForeignKey(Restaurant, on_delete=models.CASCADE)

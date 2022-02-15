97c50ee992f2d2e952f3cf456a1f852608f10e1b
nigussolomon <nigussaved@gmail.com>
2022-02-15 16:29:06 +0300

    made changes to ui from user comments

---
+++ menu/static/menu/js.js
@@ -0,0 +1,9 @@
+var update = document.getElementById("registerForm");
+var updateBtn = document.getElementById("updateBtn");
+
+updateBtn.onclick = () => {
+    update.style.transitionDelay = ".2s";
+    update.style.transitionDuration = ".3s";
+    update.style.top = "9%";
+}
+

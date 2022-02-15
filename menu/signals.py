20d5d2156685d511b339163f3fd23dde76b8d6fe
nigussolomon <nigussaved@gmail.com>
2022-02-15 18:05:21 +0300

    completed the ui for registering a restaurant

--- menu/signals.py
+++ menu/signals.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-from . models import Profile
+from . models import Profile, Restaurant
 
 @receiver(post_save, sender = User)
 def create_profile(sender, instance, created, **kwargs):
@@ -12,3 +12,12 @@ def create_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender = User)
 def create_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+@receiver(post_save, sender = User)
+def create_rest(sender, instance, created, **kwargs):
+    if created:
+        Restaurant.objects.create(user = instance)
+        
+@receiver(post_save, sender = User)
+def create_rest(sender, instance, **kwargs):
+    instance.restaurant.save()
--- menu/static/menu/js.js
+++ menu/static/menu/js.js
@@ -1,9 +1,32 @@
 var update = document.getElementById("registerForm");
 var updateBtn = document.getElementById("updateBtn");
+var addBtn = document.getElementById("addrestaurant");
+var restaurant = document.getElementById("restaurant");
+var up = document.getElementById("up");
 
 updateBtn.onclick = () => {
     update.style.transitionDelay = ".2s";
     update.style.transitionDuration = ".3s";
     update.style.top = "9%";
+    restaurant.style.top = "-100%";
 }
+
+addBtn.onclick = () => {
+    restaurant.style.transitionDelay = ".2s";
+    restaurant.style.transitionDuration = ".3s";
+    restaurant.style.top = "9%";
+    update.style.top = "-100%";
+
+}
+
+up.onclick = () =>{
+    restaurant.style.top = "9%"
+    document.location.hash = 'form';
+}
+
+window.onload = () =>{
+    restaurant.style.top = "9%"
+}
+
+
 

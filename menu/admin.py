33e3501f533a244eff30e8c8c8f88118dc95de3e
tmc <hawibek223@gmail.com>
2022-02-15 11:03:36 +0300

    completed the menu database issue

--- menu/admin.py
+++ menu/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from . models import Profile,Restaurant
+from . models import Profile,Restaurant, Menu
 
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Restaurant)
+admin.site.register(Menu)
